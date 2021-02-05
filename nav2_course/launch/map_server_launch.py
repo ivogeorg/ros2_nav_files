68e7957f122b87c075fe0627912bbadaa02b1f3c
aezquerro <albertoezquerro@gmail.com>
2021-02-05 15:38:00 +0000

    Updated repo

--- nav2_course/launch/map_server_launch.py
+++ nav2_course/launch/map_server_launch.py
@@ -22,19 +22,20 @@ def generate_launch_description():
                        ]),
 
         Node(
+            package='rviz2',
+            node_executable='rviz2',
+            node_name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            parameters=[{'use_sim_time': True}],
+            output='screen'
+            ),
+
+        Node(
             package='nav2_lifecycle_manager',
             node_executable='lifecycle_manager',
             node_name='lifecycle_manager_localization',
             output='screen',
             parameters=[{'use_sim_time': True},
                         {'autostart': True},
-                        {'node_names': ['map_server']}]),
-
-        Node(
-            package='rviz2',
-            node_executable='rviz2',
-            node_name='rviz2',
-            arguments=['-d', rviz_config_dir],
-            parameters=[{'use_sim_time': True}],
-            output='screen')
+                        {'node_names': ['map_server']}])            
         ])
